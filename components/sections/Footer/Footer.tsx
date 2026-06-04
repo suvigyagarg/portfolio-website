c0573af9dae8707cafddf67641c2a1f913cf5e70
Suvigya Garg <111561847+suvigyagarg@users.noreply.github.com>
2026-06-04 11:16:03 +0530

    chore :style

--- components/sections/Footer/Footer.tsx
+++ components/sections/Footer/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <p className={styles.sig}>
           <span> Suvigya Garg</span>
            <a href="tel:+918178029305">+91 - 8178029305</a>
-            <a href="mailto:suvigya.2003.garg@gmail.com">suvigya.2003.garg@gmail.com</a>
+            <a href="mailto:suvigyagarg178@gmail.com">suvigyagarg178@gmail.com</a>
 
           </p>
         <p>© {new Date().getFullYear()}<br/>
